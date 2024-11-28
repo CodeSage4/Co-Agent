eb0883ea486c24f5e739961e17595b15ebe257cf
Dhruv Kumar <141584754+SnookyDru@users.noreply.github.com>
2024-11-28 23:28:04 +0530

    Update config.py

--- co_agent/config.py
+++ co_agent/config.py
@@ -1,12 +1,12 @@
 # linkedin_summarizer/config.py
 from dotenv import load_dotenv, find_dotenv
 import os
-
+import streamlit as st_c
 load_dotenv(find_dotenv())
 
 llm_config = {
     "model": "gemini-1.5-flash",
-    "api_key": "AIzaSyBSSzCI4YZZSqj1EvZkBkXO-7hW-3Rioj0", #os.getenv("GOOGLE_API_KEY"),
+    "api_key": st_c.secrets["google_api_key"]
     "temperature": 0.7,
     "max_tokens": 200,
     "max_retries": 2
